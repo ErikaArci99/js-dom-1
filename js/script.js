101b1ae5d2cae38d1f49830f5b2980077a1e5df1
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-25 19:19:58 +0100

    [UPD] aggiunta interazione bottone

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ const lamp = document.getElementById('lamp');
 
 // richiamo il bottone
 const button = document.getElementById('button');
+
+
+// al click del bottone succede
+button.addEventListener('click', () => {
+    // se gli elementi sono OFF diventano ON
+    if (lamp.classList.contains('off')){
+        lamp.classList.remove('off');
+        lamp.classList.add('on');
+        lamp.src = './img/yellow_lamp.png' 
+    }
+    // altrimenti diventano ON
+    else{
+        lamp.classList.remove('on');
+        lamp.classList.add('off');
+        lamp.src = './img/white_lamp.png';
+    }
+})
